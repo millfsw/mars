d39b7a81871c5f61b2384e63f77a91af08275b18
Milana Semenova <smilana2508@yandex.ru>
2024-03-13 19:16:47 +0300

    Update mars.py

--- mars.py
+++ mars.py
@@ -1,1 +1,269 @@
+from flask import Flask, request, url_for
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def site1():
+    return """
+        <!doctype html>
+        <html lang="en">
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        </head>
+        <body>
+        <h1>Миссия Колонизация Марса</h1>
+        </body>
+        </html>
+    """
+
+
+@app.route("/index")
+def site2():
+    return """
+        <!doctype html>
+        <html lang="en">
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        </head>
+        <body>
+        <h1>И на Марсе будут яблони цвести!</h1>
+        </body>
+        </html>
+    """
+
+
+@app.route("/promotion")
+def site3():
+    return """
+        <!doctype html>
+        <html lang="en">
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        </head>
+        <body>
+        <h2>Человечество вырастает из детства.<br>Человечеству мала одна планета.<br>Мы сделаем обитаемыми безжизненные пока планеты.<br>И начнем с Марса!<br>Присоединяйся!</h2>
+        </body>
+        </html>
+    """
+
+
+@app.route("/image_mars")
+def site4():
+    return """
+        <!doctype html>
+        <html lang="en">
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Привет, Марс!</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        </head>
+        <body>
+        <h1>Жди нас, Марс!</h1>
+        <img src="static/image/MARS.png">
+        <p>Вот она какая, красная планета.</p>
+        </body>
+        </html>
+    """
+
+
+@app.route("/promotion_image")
+def site5():
+    return """
+        <!doctype html>
+        <html lang="en">
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Колонизация Марса</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="static/css/style.css">
+        </head>
+        <body>
+        <h1>Жди нас, Марс!</h1>
+        <img src="static/image/MARS.png">
+        <h2 class="text1">Человечество вырастает из детства.</h2>
+        <h2 class="text2">Человечеству мала одна планета.</h2>
+        <h2 class="text1">Мы сделаем обитаемыми безжизненные пока планеты.</h2>
+        <h2 class="text3">И начнем с Марса!<h2>
+        <h2 class="text4">Присоединяйся!</h2>
+        </body>
+        </html>
+    """
+
+
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
+def site6():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet"
+                            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                            crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Пример формы</title>
+                          </head>
+                          <body>
+                            <h1>Анкета претендента на участие в миссии</h1>
+                            <div>
+                                <form class="login_form" method="post">
+                                    <input type="text" class="form-control" id="text" aria-describedby="emailHelp" placeholder="Введите фамилию" name="text">
+                                    <input type="text" class="form-control" id="text" aria-describedby="emailHelp" placeholder="Введите имя" name="text">
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+                                    <div class="form-group">
+                                        <label for="classSelect">Какое увас образование</label>
+                                        <select class="form-control" id="classSelect" name="class">
+                                          <option>Начальное</option>
+                                          <option>Среднее</option>
+                                          <option>Высшее</option>
+                                        </select>
+                                     </div>
+                                    <div class="form-group">
+                                        <label for="form-check">Какие у Вас есть профессии?</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="work" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Инженер-исследователь
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="work" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Инженер-строитель
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="work" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Пилот
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="work" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Метеоролог
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="work" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Инженер по жизнеобеспечению
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="work" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Инженер по радиайионной защите
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="work" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Врач
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="checkbox" name="work" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Экзобиолог
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="form-check">Укажите пол</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="about">Почему Вы хотите принять участие?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label><br>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                        <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Записаться</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        print(request.form['text'])
+        print(request.form['text'])
+        print(request.form['email'])
+        print(request.form['work'])
+        print(request.form['sex'])
+        return "Форма отправлена"
+
+
+@app.route("/choice/<planet_name>")
+def site7(planet_name):
+    return """
+        <!doctype html>
+        <html lang="en">
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Колонизация Марса</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        </head>
+        <body>
+        <h1>Мое предложение: {planet_name}</h1>
+        <h2>Эта планета близка к Земле;</h2>
+        <h2 style="background-color:lightgreen;">На ней много необходимых ресурсов;</h2>
+        <h2 style="background-color:lightgrey;">На ней есть вода и атмосфера;</h2>
+        <h2 style="background-color:lightyellow;">На ней есть небольшое магнитное поле;<h2>
+        <h2 style="background-color:lightcoral;">Наконец, она просто красива!</h2>
+        </body>
+        </html>
+    """.format(planet_name=planet_name)
+
+
+@app.route("/choice/<nickname>/<int:level>/<float:rating>")
+def site8(nickname, level: int, rating: float):
+    return """
+        <!doctype html>
+        <html lang="en">
+        <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Колонизация Марса</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        </head>
+        <body>
+        <h1>Результаты отбора</h1>
+        <h2>Претендента на участие в миссии {nickname}:</h2>
+        <h2 style="background-color:lightgreen;">Поздравляем! Ваш рейтинг после {level} этапа отбора</h2>
+        <h2>составляет {rating}!</h2>
+        <h2 style="background-color:lightyellow;">Желаем удачи!<h2>
+        </body>
+        </html>
+    """.format(nickname=nickname, level=level, rating=rating)
