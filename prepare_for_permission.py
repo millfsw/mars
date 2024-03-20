57437cd107fc45be6e2a3c98dc8c09d55dce5da1
Milana Semenova <smilana2508@yandex.ru>
2024-03-20 20:36:17 +0300

    Delete prepare_for_permission.py

--- prepare_for_permission.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/<title>')
-@app.route('/index/<title>')
-def index(title):
-    return render_template("base.html", title=title)
