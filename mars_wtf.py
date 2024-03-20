5f39dca2a3cdc7258fc4aeb1411d7f3fcc0f86ef
Milana Semenova <smilana2508@yandex.ru>
2024-03-20 20:39:30 +0300

    Add files via upload

---
+++ mars_wtf.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/<title>")
+@app.route("/index/<title>")
+def prepare_for_permission(title=""):
+    return render_template("base.html", title=title)
+
+
+@app.route("/training/<prof>")
+def traning(prof):
+    if "инженер" in prof or "строитель" in prof:
+        profession = "инженерные тренажеры"
+    else:
+        profession = "научные симуляторы"
+    return render_template("training.html", profession=profession)
+
+
